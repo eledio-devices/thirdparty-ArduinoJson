d85684ebaa44345dbd94c2fe7fb9546454ed560f
Benoit Blanchon <git@benoitblanchon.fr>
2022-03-29 13:03:07 +0200

    Remove nesting() from VariantData (6796,5602)

--- src/ArduinoJson/Variant/VariantData.hpp
+++ src/ArduinoJson/Variant/VariantData.hpp
@@ -97,6 +97,10 @@ class VariantData {
     return const_cast<VariantData *>(this)->asArray();
   }
 
+  const CollectionData *asCollection() const {
+    return isCollection() ? &_content.asCollection : 0;
+  }
+
   CollectionData *asObject() {
     return isObject() ? &_content.asCollection : 0;
   }
@@ -259,12 +263,6 @@ class VariantData {
       default:
         return 0;
     }
-  }
-
-  size_t nesting() const {
-    if (isPointer())  // P+0 G+0
-      return _content.asPointer->nesting();
-    return isCollection() ? _content.asCollection.nesting() : 0;
   }
 
   size_t size() const {
--- src/ArduinoJson/Variant/VariantRef.hpp
+++ src/ArduinoJson/Variant/VariantRef.hpp
@@ -40,8 +40,8 @@ class VariantRefBase : public VariantTag {
     return _data ? _data->memoryUsage() : 0;
   }
 
-  FORCE_INLINE size_t nesting() const {
-    return _data ? _data->nesting() : 0;
+  size_t nesting() const {
+    return variantNesting(_data);
   }
 
   size_t size() const {
