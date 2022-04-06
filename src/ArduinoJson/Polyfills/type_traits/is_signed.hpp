3f8f333c0610661487967d44c40da593b6602906
Benoit Blanchon <git@benoitblanchon.fr>
2022-04-06 18:25:26 +0200

    Simplify is_signed and is_unsigned

--- src/ArduinoJson/Polyfills/type_traits/is_signed.hpp
+++ src/ArduinoJson/Polyfills/type_traits/is_signed.hpp
@@ -4,27 +4,14 @@
 
 #pragma once
 
-#include "integral_constant.hpp"
-#include "is_same.hpp"
 #include "remove_cv.hpp"
 
 namespace ARDUINOJSON_NAMESPACE {
 
-// clang-format off
 template <typename T>
-struct is_signed : integral_constant<bool, 
-    is_same<typename remove_cv<T>::type, char>::value ||
-    is_same<typename remove_cv<T>::type, signed char>::value ||
-    is_same<typename remove_cv<T>::type, signed short>::value ||
-    is_same<typename remove_cv<T>::type, signed int>::value ||
-    is_same<typename remove_cv<T>::type, signed long>::value ||
-#if ARDUINOJSON_HAS_LONG_LONG
-    is_same<typename remove_cv<T>::type, signed long long>::value ||
-#endif
-#if ARDUINOJSON_HAS_INT64
-    is_same<typename remove_cv<T>::type, signed __int64>::value ||
-#endif
-    is_same<typename remove_cv<T>::type, float>::value ||
-    is_same<typename remove_cv<T>::type, double>::value> {};
-// clang-format on
+struct is_signed {
+  typedef typename remove_cv<T>::type type;
+  static const bool value = type(-1) < type(0);
+};
+
 }  // namespace ARDUINOJSON_NAMESPACE
--- src/ArduinoJson/Polyfills/type_traits/is_unsigned.hpp
+++ src/ArduinoJson/Polyfills/type_traits/is_unsigned.hpp
@@ -4,25 +4,14 @@
 
 #pragma once
 
-#include "integral_constant.hpp"
-#include "is_same.hpp"
 #include "remove_cv.hpp"
 
 namespace ARDUINOJSON_NAMESPACE {
 
-// clang-format off
 template <typename T>
-struct is_unsigned : integral_constant<bool,
-    is_same<typename remove_cv<T>::type, unsigned char>::value ||
-    is_same<typename remove_cv<T>::type, unsigned short>::value ||
-    is_same<typename remove_cv<T>::type, unsigned int>::value ||
-    is_same<typename remove_cv<T>::type, unsigned long>::value ||
-#if ARDUINOJSON_HAS_INT64
-    is_same<typename remove_cv<T>::type, unsigned __int64>::value ||
-#endif
-#if ARDUINOJSON_HAS_LONG_LONG
-    is_same<typename remove_cv<T>::type, unsigned long long>::value ||
-#endif
-    is_same<typename remove_cv<T>::type, bool>::value> {};
-// clang-format on
+struct is_unsigned {
+  typedef typename remove_cv<T>::type type;
+  static const bool value = type(-1) > type(0);
+};
+
 }  // namespace ARDUINOJSON_NAMESPACE
